eb04832f1166d68a95d1674019ece707a6749a0d
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-04-13 11:51:27 +0530

    Refs and Controlled Components
    useRef is used to submit the input details. useState is removed(which was used for input details submission)

--- UserForm.js
+++ UserForm.js
@@ -1,36 +1,60 @@
-import React, { useState } from 'react'
-// import SubmittedDetails from './SubmittedDetails'
+import React, { Fragment, useRef, useState } from 'react'
+import ErrorModel from './ErrorModel'
+import "./Form.css"
 const UserForm = (props) => {
-    const[oldName,updateName]=useState('')
-    const[oldAge,updateAge]=useState('')
-    function updatingName(e) {
-        updateName(e.target.value)
-    }
-    function updatingAge(e) {
-        updateAge(e.target.value)
-    }
 
+    const nameInputRef=useRef('')
+    const ageInputRef=useRef('')
+    const collegeInputRef=useRef('')
+    const[isError,setIsError]=useState()
+    
+    function errorMessage() {
+        setIsError()        
+    }
     function submitDetails(e) {
         e.preventDefault();
-        const details={Name:oldName,Age:oldAge}
-        if(oldName&&oldAge){
+        console.log(nameInputRef.current.value);
+        const details={Name:nameInputRef.current.value, Age:ageInputRef.current.value, College:collegeInputRef.current.value ,id:Math.random().toString()}
+        if(details.Name&&details.Age&& details.College){
             props.onSubmittedDetails(details)
-        }else if(!oldName && !oldAge){
-            alert('Please fill the details')
-        }else if(!oldAge){
-            alert('Please enter the Age')
-        }else{
-            alert('Please enter the Username')
-        }   
+            nameInputRef.current.value=''
+            ageInputRef.current.value=''
+            collegeInputRef.current.value=''
+        }
+        else if(nameInputRef.current.value.trim().length===0 ||ageInputRef.current.value.trim().length===0 ||collegeInputRef.current.value.trim().length===0){
+            let notFound=''
+            if(nameInputRef.current.value.trim().length===0){
+                notFound+='"Username" '
+            }
+            if(ageInputRef.current.value.trim().length===0){
+                notFound+='"Age" '
+            }
+            if(collegeInputRef.current.value.trim().length===0){
+                notFound+='"College" '
+            }
+            setIsError({title:'Invalid Input',
+            message:`Please enter a valid ${notFound}`})
+        }  
     }
     return (
-        <form onSubmit={submitDetails}>
-            <label>Username</label>
-            <input type='text' value={oldName} onChange={updatingName}/>
-            <label>Age(Years)</label>
-            <input type='number' min='1' value={oldAge} onChange={updatingAge}/>
-            <button type='submit'>SUBMIT</button>
-        </form>
+        <Fragment>
+            {isError&&<ErrorModel title={isError.title} message={isError.message} onErrorMessage={errorMessage}/>}
+            <form onSubmit={submitDetails}>
+                <div class='formInput'>
+                    <label  for='userName'>Username</label>
+                    <input  type='text' id='userName' ref={nameInputRef}/>
+                </div>
+                <div class='formInput'>
+                    <label  for='age'>Age(Years)</label>
+                    <input  type='number' id='age' min='1' ref={ageInputRef}/>
+                </div>
+                <div class='formInput'>
+                    <label  for='college'>College</label>
+                    <input  type='text' id='college' ref={collegeInputRef}/>
+                </div>
+                <button type='submit'>SUBMIT</button>
+            </form>
+        </Fragment>
     )
 }
 
